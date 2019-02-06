0bef8aed97c94bbdb576188f46fb023e4e9275c6
ToruNiina <niina.toru.68u@gmail.com>
2019-02-06 21:17:32 +0900

    feat: add unique_resource and scope_exit

---
+++ spray/util/scope_exit.hpp
@@ -0,0 +1,47 @@
+#ifndef SPRAY_UTIL_SCOPE_EXIT_HPP
+#define SPRAY_UTIL_SCOPE_EXIT_HPP
+#include <type_traits>
+#include <utility>
+
+namespace spray
+{
+namespace util
+{
+
+template<typename EF>
+struct scope_exit
+{
+  public:
+    using exit_function_type = EF;
+
+    template<typename EFP>
+    explicit scope_exit(EFP&& func)
+        noexcept(std::is_nothrow_constructible<EF, EFP>::value ||
+                 std::is_nothrow_constructible<EF, EFP&>::value)
+        : func_(std::forward<EFP>(func))
+    {}
+    ~scope_exit() noexcept
+    {
+        if(this->execute_on_destruction_) {this->func_();}
+    }
+
+    scope_exit(scope_exit&& other)
+        noexcept(std::is_nothrow_move_constructible<exit_function_type>::value)
+        : func_(std::move(other.func_)),
+          execute_on_destruction_(other.execute_on_destruction_)
+    {}
+    scope_exit(const scope_exit&)            = delete;
+    scope_exit& operator=(const scope_exit&) = delete;
+    scope_exit& operator=(scope_exit&&)      = delete;
+
+    void release() noexcept {this->execute_on_destruction_ = false;}
+
+  private:
+
+     exit_function_type func_;
+     bool execute_on_destruction_;
+};
+
+} // util
+} // spray
+#endif// SPRAY_UTIL_SCOPE_EXIT_HPP
---
+++ spray/util/unique_resource.hpp
@@ -0,0 +1,105 @@
+#ifndef SPRAY_UTIL_UNIQUE_RESOURCE_HPP
+#define SPRAY_UTIL_UNIQUE_RESOURCE_HPP
+#include <type_traits>
+#include <utility>
+
+namespace spray
+{
+namespace util
+{
+
+template<typename R, typename D>
+struct unique_resource
+{
+  public:
+    using resource_type = R;
+    using deleter_type  = D;
+
+  public:
+
+    unique_resource(resource_type&& resouce, deleter_type&& deleter,
+                    bool deletion_needed = true)
+        noexcept(std::is_nothrow_move_constructible<resource_type>::value &&
+                 std::is_nothrow_move_constructible<deleter_type>::value)
+        : resource_(std::move(resource)), deleter_ (std::move(deleter)),
+          deletion_needed_(deletion_needed)
+    {}
+    ~unique_resource() noexcept(noexcept(this->reset()))
+    {
+        this->reset();
+    }
+
+    unique_resource(unique_resource&& other)
+        noexcept(std::is_nothrow_move_constructible<resource_type>::value &&
+                 std::is_nothrow_move_constructible<deleter_type>::value)
+        : resource_(std::move(other.resource_)),
+          deleter_(std::move(other.deleter_)),
+          deletion_needed_(other.deletion_needed)
+    {
+        other.release();
+    }
+
+    unique_resource& operator=(unique_resource&& other)
+        noexcept(std::is_nothrow_move_assignable<resource_type>::value &&
+                 std::is_nothrow_move_assignable<deleter_type>::value)
+    {
+        this->reset();
+        this->resource_ = std::move(other.resource_);
+        this->deleter_  = std::move(other.deleter_);
+        this->deletion_needed_ = other.deletion_needed;
+        other.release();
+        return *this;
+    }
+
+    unique_resource() = delete;
+    unique_resource(const unique_resource&) = delete;
+    unique_resource& operator=(const unique_resource&) = delete;
+
+    void reset() noexcept(noexcept(this->deleter_(this->resource_)))
+    {
+        if(deletion_needed_) {this->deleter_(this->resource_);}
+        this->deletion_needed_ = false;
+    }
+
+    void reset(resource_type&& new_resource)
+        noexcept(noexcept(this->deleter_(this->resource_)) &&
+                 std::is_nothrow_move_assignable<resource_type>::value)
+    {
+        if(deletion_needed_) {this->deleter_(this->resource_);}
+        this->resource_ = std::move(new_resource);
+        this->deletion_needed_ = true;
+    }
+
+    resource_type const& release() noexcept
+    {
+        this->deletion_needed_ = false;
+        return this->resource_;
+    }
+
+    operator resource_type const&() const noexcept {return this->resource_;}
+
+    resource_type const& get()         const noexcept {return this->resource_;}
+    deleter_type  const& get_deleter() const noexcept {return this->deleter_;}
+
+  private:
+
+    resource_type resource_;
+    deleter_type  deleter_;
+    bool deletion_needed_;
+};
+
+template<typename R, typename D>
+auto make_unique_resource(R&& resource, D&& deleter)
+{
+    static_assert(std::is_rvalue_reference<R>::value,
+        "unique_resource only accepts an r-value reference of a resource");
+    static_assert(std::is_rvalue_reference<D>::value,
+        "unique_resource only accepts an r-value reference of a deleter");
+    using result_type = unique_resource<
+        std::remove_reference_t<R>, std::remove_reference_t<D>>;
+    return result_type(std::move(resource), std::move(deleter));
+}
+
+} // util
+} // spray
+#endif // SPRAY_UTIL_UNIQUE_RESOURCE_HPP
