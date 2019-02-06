f71b1a43c1c27bd93284d22fe995ffea5dd67d0f
ToruNiina <niina.toru.68u@gmail.com>
2019-02-06 21:31:26 +0900

    fix: correct invalid include/library settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
-#include <spray/sdl/window.hpp>
-#include <spray/sdl/texture.hpp>
-#include <spray/sdl/resource.hpp>
 #include <chrono>
 #include <thread>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 #include <imgui.h>
 #include <imgui_impl_sdl.h>
 #include <imgui_impl_opengl3.h>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 
 int main()
 {
