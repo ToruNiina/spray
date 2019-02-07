01bab0c425a1894540dda3af8dedcdd5ca6753a6
ToruNiina <niina.toru.68u@gmail.com>
2019-02-07 12:21:49 +0900

    feat: open window using glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include <chrono>
-#include <thread>
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <spray/glfw/init.hpp>
+#include <spray/glfw/window.hpp>
 
 #include <imgui.h>
 #include <imgui_impl_sdl.h>
@@ -10,5 +10,13 @@
 
 int main()
 {
+    const auto glfw   = spray::glfw::init();
+    const auto window = spray::glfw::window<std::unique_ptr>(640, 480, "spray");
+
+    spray::glfw::make_context_current(window);
+    while(!spray::glfw::should_close(window))
+    {
+        glfwPollEvents();
+    }
     return 0;
 }
