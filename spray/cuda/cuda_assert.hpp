c98ca92283fa5b8315cb0e0d0f38bce543e6e8eb
ToruNiina <niina.toru.68u@gmail.com>
2019-02-09 19:49:11 +0900

    fix: avoid use of some portion of STL in cuda
    Since some of the standard template library header uses SIMD intrinsics
    (depends on the implementation) that cannot be recognized by nvcc, we
    may not be able to use it in *.cu file. `cuda_assert` may be called by
    functions that should be compiled as cuda source code. To avoid error in
    nvcc, remove most of the header from cuda_assert.

--- spray/cuda/cuda_assert.hpp
+++ spray/cuda/cuda_assert.hpp
@@ -1,7 +1,8 @@
 #ifndef SPRAY_CUDA_CUDA_ASSERT_HPP
 #define SPRAY_CUDA_CUDA_ASSERT_HPP
-#include <spray/util/log.hpp>
 #include <cuda_runtime.h>
+#include <stdexcept>
+#include <string>
 
 namespace spray
 {
@@ -12,10 +13,11 @@ inline void cuda_assert(cudaError_t err)
 {
     if(err != cudaSuccess)
     {
-        spray::log(spray::log_level::error, "{} ({}) occured.",
-                   cudaGetErrorName(err), cudaGetErrorString(err));
-        throw std::runtime_error(fmt::format("Error: {} ({})",
-                cudaGetErrorName(err), cudaGetErrorString(err)));
+        std::string description("Error(");
+        description += std::string(cudaGetErrorName(err));
+        description += "): ";
+        description += std::string(cudaGetErrorString(err));
+        throw std::runtime_error(std::move(description));
     }
     return;
 }
