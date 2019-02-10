30af047e0eedc1c083e7185d929473969fc849eb
ToruNiina <niina.toru.68u@gmail.com>
2019-02-10 15:20:36 +0900

    feat: add simplest material

---
+++ spray/core/material.hpp
@@ -0,0 +1,17 @@
+#ifndef SPRAY_CORE_MATERIAL_HPP
+#define SPRAY_CORE_MATERIAL_HPP
+#include <spray/geom/color.hpp>
+
+namespace spray
+{
+namespace core
+{
+
+struct material
+{
+    spray::geom::color albedo;
+};
+
+} // core
+} // spray
+#endif// SPRAY_CORE_MATERIAL_HPP
