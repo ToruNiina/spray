4184cb29b46896d3f89426751ca8e446f2dfd21e
ToruNiina <niina.toru.68u@gmail.com>
2019-02-27 21:08:30 +0900

    refactor: receive array of colors, not pixels

--- spray/core/show_image.cuh
+++ spray/core/show_image.cuh
@@ -1,5 +1,6 @@
 #ifndef SPRAY_CORE_SHOW_IMAGE_CUH
 #define SPRAY_CORE_SHOW_IMAGE_CUH
+#include <spray/core/color.hpp>
 #include <thrust/host_vector.h>
 #include <thrust/device_ptr.h>
 #include <vector_types.h>
@@ -12,7 +13,7 @@ namespace core
 void show_image(const dim3 blocks, const dim3 threads,
         const cudaStream_t stream, const cudaArray_const_t& buf,
         const std::size_t  width,  const std::size_t height,
-        thrust::device_ptr<const uchar4> image);
+        thrust::device_ptr<const spray::core::color> scene);
 
 } // core
 } // spray
